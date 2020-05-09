68f1f364a42c8ca3fe0d8b5b187c374445bab51f
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-05-09 12:32:13 -0500

    Login view pretty

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -7,13 +7,13 @@ export default function Login() {
     <div className='Login'>
       <div className='container-login100'>
         {" "}
-        <div className='wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30'>
+        <div className='wrap-login100 p-l-20 p-r-20 p-t-30 p-b-30'>
           {" "}
           <form className='login100-form validate-form'>
             {" "}
             <span className='login100-form-title p-b-37'> Sign In </span>{" "}
             <div
-              className='wrap-input100 validate-input m-b-20'
+              className='wrap-input100 validate-input m-b-15'
               data-validate='Enter username or email'
             >
               {" "}
@@ -26,7 +26,7 @@ export default function Login() {
               <span className='focus-input100'></span>{" "}
             </div>{" "}
             <div
-              className='wrap-input100 validate-input m-b-25'
+              className='wrap-input100 validate-input m-b-15'
               data-validate='Enter password'
             >
               {" "}
@@ -42,11 +42,11 @@ export default function Login() {
               {" "}
               <button className='login100-form-btn'> Sign In </button>{" "}
             </div>{" "}
-            <div className='text-center p-t-57 p-b-20'>
+            <div className='text-center p-t-20 p-b-10'>
               {" "}
               <span className='txt1'> Or login with </span>{" "}
             </div>{" "}
-            <div className='flex-c p-b-112'>
+            <div className='flex-c p-b-20'>
               {" "}
               <a
                 href='http://localhost:3001/auth/facebook'
@@ -63,6 +63,14 @@ export default function Login() {
                 ></i>{" "}
               </a>{" "}
             </div>{" "}
+            <div className='text-center'>
+              <p>
+                Not a member?{" "}
+                <a href='/signup' id='signup-link'>
+                  Sign up now
+                </a>
+              </p>
+            </div>
           </form>{" "}
         </div>{" "}
       </div>
