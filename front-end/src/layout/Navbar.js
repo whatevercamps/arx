808a391b10804ecd8fbd0b3356190546bf1b303e
jc-sanchez16 <32427014+jc-sanchez16@users.noreply.github.com>
2020-05-28 23:04:53 -0500

    Update Navbar.js

--- front-end/src/layout/Navbar.js
+++ front-end/src/layout/Navbar.js
@@ -1,3 +1,4 @@
+//hace falta definir los prop types para evitar errores
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
